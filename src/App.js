e605cb5f6b255e6461a9ba55574f6da3f1fec23b
Norawit Paha <6310110230@psu.ac.th>
2022-08-04 08:47:37 +0700

    part 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import CharacterCard from './CharacterCard';
 
 function App() {
   return (
-    <div className="App">
-      Hello world
+    <div >
+      <CharacterCard/>
     </div>
   );
 }
